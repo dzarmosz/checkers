66505770ec9fb7e4583810962c2a9701d4f7dee9
Krystian <k.jarmoszka@erunner.eu>
2013-09-05 10:23:09 +0200

    sword and mace

--- src/Erunner/Checkers/Tests/Cart/WeaponTest.php
+++ src/Erunner/Checkers/Tests/Cart/WeaponTest.php
@@ -4,7 +4,7 @@ use Erunner\Checkers\Product\Weapon;
 class  WeaponTest extends \PHPUnit_Framework_TestCase
 {
     protected $weapon;
-
+//---------------------TEST SWORD--------------------
     public function testSetMaterial()
     {
         $dragonSlayer = new \Erunner\Checkers\Product\Sword("Dragon Slayer", 100, "PriceForRegularClient");
@@ -25,5 +25,28 @@ class  WeaponTest extends \PHPUnit_Framework_TestCase
         $this->weapon->setWeight(1100);
 
         $this->assertEquals($dragonSlayer->getWeight(), $this->weapon->getWeight());
+    }
+
+    public function testSetWidthBlade()
+    {
+        $dragonSlayer = new \Erunner\Checkers\Product\Sword("Dragon Slayer", 1000, "PriceForNormalClient");
+        $dragonSlayer->setWidthBlade(135);
+
+        $this->weapon = new \Erunner\Checkers\Product\Sword("Dragon Slayer", 1000, "PriceForNormalClient");
+        $this->weapon->setWidthBlade(135);
+
+        $this->assertEquals($dragonSlayer->getWidthBlade(), $this->weapon->getWidthBlade());
+    }
+//-------------------TEST MACE--------------------
+
+    public function testSetNumberOfSpike()
+    {
+        $arcanaMace = new \Erunner\Checkers\Product\Mace("Arcana Mace", 400, "PriceForRegularClient");
+        $arcanaMace->setNumberOfSpike(5);
+
+        $this->weapon = new \Erunner\Checkers\Product\Mace("Arcana Mace", 400, "PriceForRegularClient");
+        $this->weapon->setNumberOfSpike(5);
+
+        $this->assertEquals($arcanaMace->getNumberOfSpike(), $this->weapon->getNumberOfSpike());
     }
 }
